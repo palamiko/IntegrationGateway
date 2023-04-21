377b08f0a815e796a66228b245e6474824c374d1
Илья <lil_wanted@mail.ru>
2023-04-21 15:30:44 +0600

    ver 0.4 - Методы ZD перенесены в отдельный модуль - Для методов Jira добавлена обработка ошибок и логирование - Прочие мелкие изменения

--- app/conf/config.py
+++ app/conf/config.py
@@ -5,13 +5,13 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "IntegrationGateway"
 
-    jira_url: str = ''
-    jira_login: str = ''
-    jira_pass: str = ''
+    jira_url: str = 'https://support.ai-mark.info'
+    jira_login: str = 'support_l3'
+    jira_pass: str = 'Ya4ScWZ8'
 
-    zendesk_url: str = ''
-    zendesk_login: str = ''
-    zendesk_pass: str = ''
+    zendesk_url: str = 'https://turon.zendesk.com'
+    zendesk_login: str = 'n.kiselev@ai-mark.ru/token'
+    zendesk_pass: str = '5E6EvFz2KpPFx9nUzxcSGdliutX2ruwa5TkUVYJn'
 
     def jira_auth(self):
         return BasicAuth(self.jira_login, self.jira_pass)
